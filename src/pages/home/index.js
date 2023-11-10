1b86fce0a66995d9b58f6b43a618cdd8d38f19b0
tamij12 <tamijangosrz@gmail.com>
2023-11-09 18:49:53 -0600

    d-sectionmain

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Posts from "../posts/posts";
-import SectionMain from "../../components/SectionMain/SectionMain";
 
 export default function Home() {
   return (
     <React.Fragment>
-      {/* <SectionMain/> */}
       <Posts />
     </React.Fragment>
   );
